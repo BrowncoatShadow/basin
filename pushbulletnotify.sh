c8240ab552c5b371ed7546350fcd4d0538b82c59
BrowncoatShadow <BrowncoatShadow@users.noreply.github.com>
2015-03-11 10:20:54 -0500

    redirect curl output to null so cronjobs do not send user mail

--- pushbulletnotify.sh
+++ pushbulletnotify.sh
@@ -7,7 +7,7 @@ source $HOME/.config/twitcheckrc
 if [ -n "$PB_TARGET" ]
 	then
 		# Push to target device.
-		curl --header 'Authorization: Bearer '$PB_TOKEN'' -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"device_iden": "'"$PB_TOKEN"'", "type": "link", "title": "'"$1"' is streaming '"$2"'", "body": "'"$3"'", "url": "'"$4"'"}'
+		curl --header 'Authorization: Bearer '$PB_TOKEN'' -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"device_iden": "'"$PB_TOKEN"'", "type": "link", "title": "'"$1"' is streaming '"$2"'", "body": "'"$3"'", "url": "'"$4"'"}' > /dev/null
 	else
 		# Push to all devices, if no target.
-		curl --header 'Authorization: Bearer '$PB_TOKEN'' -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "link", "title": "'"$1"' is streaming '"$2"'", "body": "'"$3"'", "url": "'"$4"'"}'
+		curl --header 'Authorization: Bearer '$PB_TOKEN'' -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "link", "title": "'"$1"' is streaming '"$2"'", "body": "'"$3"'", "url": "'"$4"'"}' > /dev/null
