b6b8aa41ba66139ed9de99d89148529591d764d9
Crendgrim <crendgrim@gmx.de>
2015-03-12 21:58:49 +0100

    add icon to kdialog notifications

--- modules/kdialog_notify.sh
+++ modules/kdialog_notify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # kdialog notification module for twitcheck by Crendgrim
 
-kdialog --title "$1 is now live" --passivepopup "[$2] $3" 10
+kdialog --title "$1 is now live" --icon "video-player" --passivepopup "[$2] $3"
