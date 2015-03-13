6fd6333c4fc42f5723aa0e037bf296f2884afe75
BrowncoatShadow <BrowncoatShadow@users.noreply.github.com>
2015-03-12 22:29:07 -0500

    Created setting to use native applescript.
    This new setting allows for users to setup OS X notifications without installing an extra dependency. However, this sacrifices the ability to click the notification to launch the stream in browser.

--- modules/osx_notify.sh
+++ modules/osx_notify.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 # OS X Notification Center module for twitcheck by BrowncoatShadow and Crendgrim
-# Requires: terminal-notifier <https://github.com/alloy/terminal-notifier>
+# Optional: terminal-notifier <https://github.com/alloy/terminal-notifier>
 
-terminal-notifier -message "$3" -title "Twitch" -subtitle "$1 is now streaming $2" -open "$4"
+# Fetch settings
+source $HOME/.config/twitcheckrc
+
+# Check which notification type to use.
+if [ "$OSX_TERMNOTY" == "true" ]
+then
+
+	# Send using terminal-notifier
+	terminal-notifier -message "$3" -title "Twitch" -subtitle "$1 is now streaming $2" -open "$4"
+
+else
+
+	# Send using OS X applescript (no URL support)
+	osascript -e 'display notification "$3" with title "Twitch" subtitle "$1 is now streaming $2"'
+fi
