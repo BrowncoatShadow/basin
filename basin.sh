8fe2118df6cf265f12b4425c3038735e02b1f651
BrowncoatShadow <BrowncoatShadow@users.noreply.github.com>
2015-07-02 21:38:27 -0500

    Moved modules to functions within the main script.

--- basin.sh
+++ basin.sh
@@ -145,6 +145,90 @@ check_file $DBFILE "{}"
 
 # BEGIN FUNCTIONS
 
+# BEGIN NOTIFIERS
+# Echo notifier.
+echo_notify() {
+	echo "$1 [$2]: $3 <$4>"
+}
+
+# Kdialog notifier.
+kdialog_notify() {
+	kdialog --title "$1 is now live on $5" --icon "video-player" --passivepopup "<b>$2</b><br>$3"
+}
+
+# OS X notifier.
+osx_notifiy() {
+	# Check which notification type to use.
+	if [ "$OSX_TERMNOTY" == "true" ]
+	then
+
+		# Send using terminal-notifier
+		terminal-notifier -message "$3" -title "Twitch" -subtitle "$1 is now streaming $2" -open "$4"
+
+	else
+
+		# Send using OS X applescript (no URL support)
+		osascript -e "display notification \"$3\" with title \"Twitch\" subtitle \"$1 is now streaming $2\""
+	fi
+}
+
+# Pushbullet notifier.
+pb_notify() {
+	# Make sure we have a token.
+	if [[ -z "$PB_TOKEN" ]]
+	then
+		echo "ERROR You need to set PB_TOKEN in your settings file." >&2
+		exit 1
+	fi
+	
+	# Place the arguments from script into variables that the functions can use.
+	name="$1"; game="$2"; stat="$3"; url="$4"; uri="twitch://stream/$1"
+	
+	# Create functions for sending pushes. 
+	allpush() {
+		curl -s --header 'Authorization: Bearer '$PB_TOKEN'' -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "link", "title": "'"$name"'", "body": "'"[$game] \\n$stat"'", "url": "'"$1"'"}' > /dev/null
+	}
+	
+	targetpush() {
+		curl -s --header 'Authorization: Bearer '$PB_TOKEN'' -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"device_iden": "'"$1"'", "type": "link", "title": "'"$name"'", "body": "'"[$game] \\n$stat"'", "url": "'"$2"'"}' > /dev/null
+	}
+	
+	# If no targets are defined, send to all targets.
+	if [[ -z "$PB_URLTARGET" && -z "$PB_URITARGET" ]]
+	then
+		# If PB_URI is true, send the uri link instead of url.
+		if [ "$PB_ALLURI" == "true" ]
+		then
+			link="$uri"
+		else
+			link="$url"
+		fi
+	
+		# Push to all.
+		allpush $link
+	fi
+	
+	# For each target in URL list, send a push.
+	if [ -n "$PB_URLTARGET" ]
+	then
+		for target in $PB_URLTARGET
+		do
+			targetpush $target $url
+		done
+	fi
+	
+	# For each target in URI list, send a push.
+	if [ -n "$PB_URITARGET" ]
+	then
+		for target in $PB_URITARGET
+		do
+			targetpush $target $uri
+		done
+	fi
+}
+# END NOTIFIERS
+
+# BEGIN SERVICES
 # Twitch service plugin
 get_channels_twitch() {
 
@@ -229,6 +313,7 @@ get_channels_hitbox() {
 	echo "$new_online_json"
 
 }
+#END SERVICES
 
 # Get data from the database.
 get_db() {
--- modules/kdialog_notify.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# kdialog notification module for twitcheck by Crendgrim
-
-kdialog --title "$1 is now live on $5" --icon "video-player" --passivepopup "<b>$2</b><br>$3"
